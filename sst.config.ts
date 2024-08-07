426bc165dc6a50aa6334812a35b3db50c0440890
Anton Prudkohliad <anton.prudkohliad@protonmail.com>
2024-08-07 19:05:36 +0200

    Add Hcloud

--- sst.config.ts
+++ sst.config.ts
@@ -1,11 +1,11 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "next-self-hosted",
       removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
+      home: "local",
+      providers: { hcloud: true },
     };
   },
   async run() {},
