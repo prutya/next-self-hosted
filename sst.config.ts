239ec3e33da9cd07b0912126cddeb523d44e03cf
Anton Prudkohliad <anton.prudkohliad@protonmail.com>
2024-08-10 11:53:48 +0200

    Run app and nginx

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,7 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-import { resolve as pathResolve } from "path";
+import { resolve as pathResolve } from "node:path";
 import { writeFileSync as fsWriteFileSync } from "node:fs";
+import { asset as pulumiAsset } from "@pulumi/pulumi";
 
 export default $config({
   app(input) {
@@ -12,6 +13,7 @@ export default $config({
         hcloud: true,
         tls: true,
         docker: true,
+        "@pulumi/command": true,
       },
     };
   },
@@ -65,6 +67,184 @@ export default $config({
         provider: dockerServerHetzner,
         dependsOn: [server],
       }
+    );
+
+    // Setup Docker Volumes
+    const dockerVolumeAppBuild = new docker.Volume(
+      "Docker Volume - App Build",
+      { name: "app_build" },
+      { provider: dockerServerHetzner, dependsOn: [server] }
+    );
+
+    // Setup Docker Networks
+    const dockerNetworkPublic = new docker.Network(
+      "Docker Network - Public",
+      { name: "app_network_public" },
+      { provider: dockerServerHetzner, dependsOn: [server] }
+    );
+    const dockerNetworkInternal = new docker.Network(
+      "Docker Network - Internal",
+      { name: "app_network_internal" },
+      { provider: dockerServerHetzner, dependsOn: [server] }
+    );
+
+    // Run a one-off container to build the app
+    const dockerAppBuildContainer = new docker.Container(
+      "Docker Container - App Build",
+      {
+        name: "app_build",
+        image: dockerImageHetzner.imageName,
+        volumes: [
+          {
+            volumeName: dockerVolumeAppBuild.name,
+            containerPath: "/app/.next",
+          },
+        ],
+        command: ["pnpm", "build"],
+        mustRun: true,
+      },
+      {
+        provider: dockerServerHetzner,
+      }
+    );
+
+    // Run the app container
+    const dockerAppContainer = new docker.Container(
+      "Docker Container - App",
+      {
+        name: "app",
+        image: dockerImageHetzner.imageName,
+        volumes: [
+          {
+            volumeName: dockerVolumeAppBuild.name,
+            containerPath: "/app/.next",
+          },
+        ],
+        networksAdvanced: [
+          { name: dockerNetworkPublic.id },
+          { name: dockerNetworkInternal.id },
+        ],
+        command: ["pnpm", "start"],
+        restart: "always",
+      },
+      { provider: dockerServerHetzner, dependsOn: [dockerAppBuildContainer] }
+    );
+
+    // Ensure app directory exists
+    new command.remote.Command("Command - Ensure app directory", {
+      create: "mkdir -p /root/app",
+      connection: {
+        host: server.ipv4Address,
+        user: "root",
+        privateKey: sshKeyLocal.privateKeyOpenssh,
+      },
+    });
+
+    // Ensure app/certs directory exists
+    new command.remote.Command("Command - Ensure app/certs directory", {
+      create: "mkdir -p /root/app/certs",
+      connection: {
+        host: server.ipv4Address,
+        user: "root",
+        privateKey: sshKeyLocal.privateKeyOpenssh,
+      },
+    });
+
+    // Copy Nginx config to the VPS
+    const commandCopyNginxConfig = new command.remote.CopyToRemote(
+      "Copy - Nginx Config",
+      {
+        source: new pulumiAsset.FileAsset(
+          pathResolve("./nginx/production.conf")
+        ),
+        remotePath: "/root/app/nginx.conf",
+        connection: {
+          host: server.ipv4Address,
+          user: "root",
+          privateKey: sshKeyLocal.privateKeyOpenssh,
+        },
+      }
+    );
+
+    // Copy Certificates to the VPS
+    const commandCopyCertificatePrivate = new command.remote.CopyToRemote(
+      "Copy - Certificates - Key",
+      {
+        source: new pulumiAsset.FileAsset(
+          pathResolve("./certs/cloudflare.key.pem")
+        ),
+        remotePath: "/root/app/certs/cloudflare.key.pem",
+        connection: {
+          host: server.ipv4Address,
+          user: "root",
+          privateKey: sshKeyLocal.privateKeyOpenssh,
+        },
+      }
+    );
+    const commandCopyCertificatePublic = new command.remote.CopyToRemote(
+      "Copy - Certificates - Cert",
+      {
+        source: new pulumiAsset.FileAsset(
+          pathResolve("./certs/cloudflare.cert.pem")
+        ),
+        remotePath: "/root/app/certs/cloudflare.cert.pem",
+        connection: {
+          host: server.ipv4Address,
+          user: "root",
+          privateKey: sshKeyLocal.privateKeyOpenssh,
+        },
+      }
+    );
+    const commandCopyCertificateAuthenticatedOriginPull =
+      new command.remote.CopyToRemote(
+        "Copy - Certificates - Authenticated Origin Pull",
+        {
+          source: new pulumiAsset.FileAsset(
+            pathResolve("./certs/authenticated_origin_pull_ca.pem")
+          ),
+          remotePath: "/root/app/certs/authenticated_origin_pull_ca.pem",
+          connection: {
+            host: server.ipv4Address,
+            user: "root",
+            privateKey: sshKeyLocal.privateKeyOpenssh,
+          },
+        }
+      );
+
+    // Run the Nginx container
+    const dockerNginxContainer = new docker.Container(
+      "Docker Container - Nginx",
+      {
+        name: "nginx",
+        image: "nginx:1.27.0-bookworm",
+        volumes: [
+          {
+            hostPath: "/root/app/nginx.conf",
+            containerPath: "/etc/nginx/nginx.conf",
+          },
+          {
+            hostPath: "/root/app/certs",
+            containerPath: "/certs",
+          },
+        ],
+        command: ["nginx", "-g", "daemon off;"],
+        networksAdvanced: [{ name: dockerNetworkPublic.id }],
+        restart: "always",
+        ports: [
+          {
+            external: 443,
+            internal: 443,
+          },
+        ],
+        healthcheck: {
+          tests: ["CMD", "service", "nginx", "status"],
+          interval: "30s",
+          timeout: "5s",
+          retries: 5,
+          startPeriod: "10s",
+        },
+      },
+      { provider: dockerServerHetzner, dependsOn: [dockerAppContainer] }
     );
 
     return { ip: server.ipv4Address };
