1e4361adea96213ab1b346d10742e6a4828b6474
Anton Prudkohliad <anton.prudkohliad@protonmail.com>
2024-09-28 22:01:37 +0200

    Add source code link

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <hr />
+        <p>
+          <Link href="https://github.com/prutya/next-self-hosted">
+            {"Source code"}
+          </Link>
+        </p>
+      </body>
     </html>
   );
 }
